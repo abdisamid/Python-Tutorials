7ab89af13f4f49f2987fee7a4d3ece0dc7dcf34f
abdisamid <abdisamidabdullahi@gmail.com>
2022-08-12 20:05:11 +0300

    Create python_Strings.py

---
+++ python_Strings.py
@@ -0,0 +1,9 @@
+st = "Hello"
+st.find("l") # this method finds the letter "l" in the string st
+st.upper() # this method changes all of our letters to uppercases => "HELLO"
+st.lower() # this changes them to lower case => "hello"
+st.capitalize() # this method capitalizes the first element in the string => "Hello"
+st.isdigit() # this outputs True if all characters in the string are digits and returns False If the string contains 1 or more non-digits.
+st.split() # this method splits the elements of a string using a delimiter/seperator
+st.replace("l",".") # this method replaces the letter l with a fullstop.
+
