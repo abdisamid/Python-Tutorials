fb77c2774f1317d28749cc1341d94368dfc2e4e8
abdisamid <abdisamidabdullahi@gmail.com>
2022-08-13 19:30:17 +0300

    Create python_WhileLoop.py

---
+++ python_WhileLoop.py
@@ -0,0 +1,31 @@
+# basic while loop syntax in python => :
+x = 0
+while x < 5:
+  x += 1
+  print(x) # this outputs => : 1 2 3 4 5 
+  
+
+count = 0
+while (count < 9):
+   print ('The count is:', count)
+   count = count + 1
+
+print ("Good bye!")
+# this outputs => :
+The count is: 0 
+The count is: 1 
+The count is: 2 
+The count is: 3 
+The count is: 4 
+The count is: 5 
+The count is: 6 
+The count is: 7 
+The count is: 8 
+Good bye!
+
+
+ 
+  # control statements => :
+  # break => this terminates the loop statement and transfers the execution to the statement immediately following the loop.
+  # continue => Causes the loop to skip the remainder of its body and immediately retest its condition prior to reiterating.
+  # pass => The pass statement in Python is used when a statement is required syntactically but you do not want any command or code to execute.
