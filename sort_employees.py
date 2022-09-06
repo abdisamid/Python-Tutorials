1a720b5637e2b0060f3a01c7e5add81da48d89c8
abdisamid <abdisamidabdullahi@gmail.com>
2022-09-06 20:45:33 +0300

    Create sort_employees.py

---
+++ sort_employees.py
@@ -0,0 +1,9 @@
+# Copyright © 2022 AlgoExpert LLC. All rights reserved.
+
+def sort_employees(employees, sort_by):
+    sort_indices = ["name", "age", "salary"]
+    sort_index = sort_indices.index(sort_by)
+
+    sorted_employees = sorted(employees, key=lambda x: x[sort_index])
+
+    return sorted_employees
